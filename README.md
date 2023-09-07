a3779b15eda40803037f6bd00b9132ee6aea9733
oganesssson <265micolca@gmail.com>
2023-09-07 01:12:29 -0400

    Start problem 1
    Added imports and the UCI dataset.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ece421-assignments
- Assignments for the 'Introduction to Machine Learning' course at the University of Toronto.
+ Assignments for the 'Introduction to Machine Learning' course at the University of Toronto taught by Prof. Nicolas Papernot.
---
+++ a1/problem1.py
@@ -0,0 +1,24 @@
+# ECE421 - Introduction to Machine Learning
+# Assignment 1
+
+import sklearn
+from sklearn.datasets import load_breast_cancer
+import numpy as np
+import matplotlib as plt
+import math
+
+# Problem 1
+# def k_means(load_breast_cancer, k): # TODO: watch lectures
+
+
+
+
+# 1.1
+'''
+(5 points) Implement κ-means yourself. Your function should take in an array containing
+a dataset and a value of κ, and return the cluster centroids along with the cluster
+assignment for each data point. You may choose the initialization heuristic of your
+choice among the two we saw in class. Hand-in the code for full credit. For this
+question, you should not rely on any library other than numPy in Python.
+'''
+
