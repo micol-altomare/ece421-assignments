b72886a30ba0d3dc4e19b900692a280aa277a566
oganesssson <265micolca@gmail.com>
2023-09-15 15:07:56 -0400

    Edit comments

--- a1/problem1.py
+++ a1/problem1.py
@@ -30,22 +30,18 @@ def k_means(dataset, k):
     for i in range(k): 
         random_index = np.random.randint(0, len(dataset.data) - 1)
         centroid_chosen = dataset.data[random_index] 
-        centroids.append(centroid_chosen) #***could clean up 
+        centroids.append(centroid_chosen) 
 
-    max_iterations = 10000
+    max_iterations = 500
     w = 0
     
     for w in range(max_iterations):
-        # while not np.allclose(centroids, prev_centroids): 
 
         assignments = [[] for i in range(k)]
 
-        print("Iteration: ", w)
-
         # Cluster assignment
-        # compute the distance between each data point and each centroid
         for point in dataset.data: 
-            distances = [np.linalg.norm(point - centroid) for centroid in centroids]
+            distances = [np.linalg.norm(point - centroid) for centroid in centroids] # compute the distance between each data point and each centroid
             centroid_index = np.argmin(distances) # the index of the centroid with the minimum distance
             assignments[centroid_index].append(point) # the point is assigned by adding it to the list of points whose centroid is the index in assignments
 
@@ -54,13 +50,12 @@ def k_means(dataset, k):
         # Move centroids
         for r in range(len(centroids)):    
             # compute the mean of the assigned data points
-            mean = np.mean(np.array(assignments[r]), dtype=np.float64, axis=(0)) # column-wise # TODO: double-check
-            # move the centroid to the mean
+            mean = np.mean(np.array(assignments[r]), dtype=np.float64, axis=(0))
             centroids[r] = mean
         
-        if np.allclose(centroids, prev_centroids, rtol=1e-09): # convergence check # TODO: use divergence instead?
-            print("Converged!")
-            break
+        # Convergence check
+        if np.allclose(centroids, prev_centroids, rtol=1e-10): 
+            break # converged
 
         w += 1
         
@@ -77,9 +72,10 @@ for i in range(2, 8):
     k_means(dataset, i)
 
 # I chose to pass in the entire dataset as the input to the k-means algorithm since
-# the dataset isn't computationally expensive and all the points can be used, via 
-# distortion to help determine the best value of k.
-
+# the dataset isn't computationally expensive and the points can be used, via 
+# distortion, to help determine the best value of k. Specifically, the dataset's shape
+# is (569, 30) where each of the 569 rows are points and each of the 30 columns are
+# traits characterizing the breast cancer data.
 
 # 1.3
 '''
@@ -93,21 +89,16 @@ def distortion(centroids, assignments):
     Compute the distortion (ie. minimize the objective) of the k-means clustering algorithm 
     given the final (post-convergence) centroids and assignments.
     '''
-    distances = [] # a list where each element is the distance between a point in the dataset and its centroid
+    distances = [] 
     total_distance = 0
     m = 0
-    
-    print(len(assignments))
-    print(len(assignments[0]))
 
     for i in range(len(centroids)): # iterate per centroid
-        # add the sum of the distances between each point (found in assignments[i]) and its centroid (centroids[i])
-        distances += [np.linalg.norm(its_point - centroids[i])**2 for its_point in assignments[i]] # TODO: verify distortion equation
+        # add the sum of squared L2 norm between each point (found in assignments[i]) and its centroid (centroids[i])
+        distances += [np.linalg.norm(its_point - centroids[i])**2 for its_point in assignments[i]] 
         m += len(assignments[i])
 
     total_distance = sum(distances)
-    print("Total distance: ", total_distance)
-    print("m: ", m)
 
     j = total_distance / m
 
@@ -119,12 +110,8 @@ k_vals = np.arange(2, 8, 1)
 j_vals = np.zeros(len(k_vals))
 
 for i in range(len(k_vals)):
-    print("=====================================")
-    print("Running k-means for k = ", k_vals[i], "...")
     centroids, assignments = k_means(dataset, k_vals[i])
     j_vals[i] = distortion(centroids, assignments)
-
-print("Done!")
 
 # Plot the k vs. distortion graph
 plt.plot(k_vals, j_vals)
@@ -132,13 +119,12 @@ plt.xlabel('k-value')
 plt.ylabel('Distortion (J)')
 plt.show()
 
-print("Actually Done!")
 
 # 1.4
 '''
 (1 point) If you had to pick one value of κ, which value would you pick? Justify your choice.
 '''
 # I'd choose k = 4 since it is the point on the graph at which the distortion begins to level 
-# off (is minimized) and when real-world constraints would likely make it impractical to have 
-# more clusters.
-# TODO: verify my answer for k
+# off (ie. the elbow joint). In real-world applications, real constraints (eg. cost) would 
+# likely make it impractical to have more clusters while having more clusters doesn't make it 
+# necessarily easier to interpret the data (depending on the dataset).
